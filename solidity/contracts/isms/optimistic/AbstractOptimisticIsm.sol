838b59f6bbf8fd9781fdd6fa885ea1767985f047
Alex Voloshko <alexander.voloshko@sony.com>
2023-10-23 02:48:05 +0200

    feat: Implement OptimisticISM for secure interchain messages
    This commit introduces OptimisticISM, a new security module for the Hyperlane interchain messaging protocol. It ensures message security using an optimistic approach, with a fraud window for additional safety checks.
    
    Changes:
    - Implemented OptimisticISM with essential verification functions.
    - Added tests to validate the implementation.

---
+++ solidity/contracts/isms/optimistic/AbstractOptimisticIsm.sol
@@ -0,0 +1,219 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.8.0;
+
+// ============ External Imports ============
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+// ============ Internal Imports ============
+import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
+import {IOptimisticIsm} from "../../interfaces/isms/IOptimisticIsm.sol";
+import "../../Mailbox.sol";
+
+/**
+ * @title AbstractOptimisticIsm
+ * @notice Manages message verification for interchain communication using an optimistic approach.
+ */
+abstract contract AbstractOptimisticIsm is IOptimisticIsm, Ownable {
+    // ============ Constants ============
+
+    // solhint-disable-next-line const-name-snakecase
+    uint8 public constant moduleType =
+        uint8(IInterchainSecurityModule.Types.OPTIMISTIC);
+
+    // ============ Virtual Functions ============
+    // ======= OVERRIDE THESE TO IMPLEMENT =======
+
+    /**
+     * @dev Gets the watchers and the threshold for a specific message.
+     * @param _message The interchain message.
+     * @return watchers An array of watcher addresses
+     * @return threshold The threshold value.
+     */
+    function watchersAndThreshold(bytes calldata _message)
+        public
+        view
+        virtual
+        returns (address[] memory watchers, uint8 threshold);
+
+    // ============ Events ============
+
+    event FraudWindowSet(uint256 fraudWindow);
+    event SubmoduleSet(address submodule, uint32 origin);
+
+    // ============= Structs =============
+
+    struct PreVerifiedMessageData {
+        // Maximize the efficiency of storage per message verification to be accommodated within a total of 32 bytes
+        address submodule;
+        uint96 timestamp;
+    }
+
+    // ============ Mutable Storage ============
+
+    // Mapping to keep track of which watcher has marked a submodule as fraudulent
+    mapping(address => mapping(address => bool)) public hasMarkedFraudulent;
+
+    // Mapping to keep track of how many times a submodule has been marked as fraudulent
+    mapping(address => uint256) public fraudulentCount;
+
+    // Mapping to store pre-verified messages along with submodule
+    mapping(bytes32 => PreVerifiedMessageData) public preVerifiedMessages;
+
+    // Mapping to store submodules per message origin
+    mapping(uint32 => IInterchainSecurityModule) public submodules;
+
+    // The duration of the fraud window
+    uint256 public fraudWindow;
+
+    // ============ Modifiers ============
+
+    modifier onlyWatcher() {
+        (address[] memory _watchers, ) = this.watchersAndThreshold("");
+
+        bool found = false;
+        for (uint256 i = 0; i < _watchers.length; i++) {
+            if (_watchers[i] == msg.sender) {
+                found = true;
+                break;
+            }
+        }
+
+        require(found, "caller is not the watcher");
+
+        _;
+    }
+
+    // ============ Public Functions ============
+
+    /**
+     * @notice Retrieves the current submodule designated for message verification.
+     * @dev The submodule can vary based on the content or context of the message being processed.
+     * @param _message Formatted Hyperlane message whose corresponding submodule is to be retrieved.
+     * @return module The ISM instance that should be used to verify the provided message.
+     */
+    function submodule(bytes calldata _message)
+        external
+        view
+        returns (IInterchainSecurityModule)
+    {
+        uint32 _origin = Message.origin(_message);
+
+        return submodules[_origin];
+    }
+
+    /**
+     * @notice Verifies a message after it has passed the fraud window period.
+     * @param _message The interchain message.
+     * @return True if the verification is successful, otherwise false.
+     */
+    function verify(bytes calldata, bytes calldata _message)
+        public
+        returns (bool)
+    {
+        bytes32 _id = Message.id(_message);
+
+        PreVerifiedMessageData memory _data = preVerifiedMessages[_id];
+
+        require(_data.timestamp > 0, "message has not been pre-verified");
+        require(
+            _data.timestamp + fraudWindow < block.timestamp,
+            "fraudWindow has not passed yet"
+        );
+
+        (, uint8 _threshold) = watchersAndThreshold(_message);
+
+        require(
+            fraudulentCount[_data.submodule] < _threshold,
+            "pre-verification submodule is fraudulent"
+        );
+
+        // Can release some GAS now as as Mailbox contract keeps a list of delivered messages.
+        delete preVerifiedMessages[_id];
+
+        return true;
+    }
+
+    /**
+     * @notice Initiates the pre-verification of interchain messages using the configured submodule
+     * @dev This function should be called before the main verification
+     * @param _message Hyperlane encoded interchain message
+     * @return True if the message successfully passes the pre-verification
+     */
+    function preVerify(bytes calldata _metadata, bytes calldata _message)
+        external
+        returns (bool)
+    {
+        bytes32 _id = Message.id(_message);
+        uint32 _origin = Message.origin(_message);
+
+        IInterchainSecurityModule _ism = submodules[_origin];
+
+        preVerifiedMessages[_id] = PreVerifiedMessageData({
+            submodule: address(_ism),
+            timestamp: uint96(block.timestamp)
+        });
+
+        require(
+            _ism.verify(_metadata, _message),
+            "message does not pass pre-verification"
+        );
+
+        return true;
+    }
+
+    /**
+     * @notice Marks a submodule as fraudulent.
+     * @param _submodule The address of the submodule to mark as fraudulent.
+     */
+    function markFraudulent(address _submodule) external override onlyWatcher {
+        require(
+            !hasMarkedFraudulent[msg.sender][_submodule],
+            "already marked as fraudulent"
+        );
+
+        // Mark that the sender has now marked this submodule as fraudulent
+        hasMarkedFraudulent[msg.sender][_submodule] = true;
+
+        // Increment the fraudulent count for this submodule
+        fraudulentCount[_submodule] += 1;
+    }
+
+    /**
+     * @notice Sets the duration of the fraud window.
+     * @param _fraudWindow The new duration of the fraud window.
+     */
+    function setFraudWindow(uint256 _fraudWindow) external onlyOwner {
+        require(_fraudWindow > 0, "fraudWindow must be positive");
+
+        fraudWindow = _fraudWindow;
+
+        emit FraudWindowSet(_fraudWindow);
+    }
+
+    /**
+     * @notice Assigns a submodule to a specific message origin.
+     * @param _submodule The address of the submodule.
+     * @param _origin The origin of the messages the submodule is responsible for.
+     */
+    function setSubmodule(address _submodule, uint32 _origin)
+        external
+        onlyOwner
+    {
+        submodules[_origin] = IInterchainSecurityModule(_submodule);
+
+        emit SubmoduleSet(_submodule, _origin);
+    }
+
+    /**
+     * @dev Retrieves the pre-verified message data.
+     * @param _messageId The identifier of the message.
+     * @return The address of the submodule and the timestamp of when the message was pre-verified.
+     */
+    function preVerifiedMessageData(bytes32 _messageId)
+        external
+        returns (address, uint96)
+    {
+        PreVerifiedMessageData memory data = preVerifiedMessages[_messageId];
+        return (data.submodule, data.timestamp);
+    }
+}
---
+++ solidity/contracts/isms/optimistic/StaticOptimisticIsm.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.8.0;
+
+// ============ Internal Imports ============
+import {AbstractOptimisticIsm} from "./AbstractOptimisticIsm.sol";
+import {AggregationIsmMetadata} from "../../libs/isms/AggregationIsmMetadata.sol";
+import {MetaProxy} from "../../libs/MetaProxy.sol";
+import "../aggregation/AbstractAggregationIsm.sol";
+
+/**
+ * @title StaticAggregationIsm
+ * @notice Manages per-domain m-of-n ISM sets that are used to verify
+ * interchain messages.
+ */
+contract StaticOptimisticIsm is AbstractOptimisticIsm {
+    // ============ Public Functions ============
+
+    /**
+     * @notice Returns the set of watchers responsible for marking fraudulent submodules and the threshold
+     * @dev Can change based on the content of _message
+     * @return watchers The array of addresses
+     * @return threshold The number of ISMs needed to verify
+     */
+    function watchersAndThreshold(bytes calldata)
+        public
+        view
+        virtual
+        override
+        returns (address[] memory, uint8)
+    {
+        return abi.decode(MetaProxy.metadata(), (address[], uint8));
+    }
+}
